04c7eb414c3106f6b394c2629abbab0f829c371c
liviakobayashii <liviakmm10@gmail.com>
2025-03-25 17:33:01 -0300

    update: atualização da estilização do código

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,40 +28,42 @@ export default function Page() {
         <h2 className="text-xl text-gray-950 font-bold py-5">
           Conheça todas as raças de cachorros em um só lugar!
         </h2>
+        {dogs.isLoading && (
+          <h2 className="text-2xl font-bold text-gray-950">Carregando...</h2>
+        )}
         {/* <Link href={"/random"}>
         <button className=" text-white bg-amber-900 hover:bg-amber-950 hover:cursor-pointer duration-200 p-3 rounded-sm">
           Gerar imagem aleatória
         </button>
       </Link> */}
 
-        <div className="flex flex-col bg-white p-6 rounded-sm">
-          {dogs.isLoading && (
-            <h2 className="text-2xl font-bold text-gray-950">Carregando...</h2>
-          )}
-          <ul className=" grid grid-cols-4 gap-3">
-            {dogs.data &&
-              Object.keys(dogs.data.message).map((item, i) => (
-                <Dialog key={i}>
-                  <DialogTrigger className="flex items-center gap-2">
-                    <li className="flex gap-2 text-gray-900 p-4 bg-neutral-100  w-full hover:bg-neutral-200 hover:cursor-pointer duration-200 rounded-sm">
-                      <Icon icon="ph:dog-bold" className="text-amber-900" />
-                      {capitalizeFirstLetter(item)}
-                    </li>
-                  </DialogTrigger>
-                  <DialogContent>
-                    <DialogHeader className="flex justify-center items-center">
-                      <DialogTitle className="py-4">
-                        Fotos da raça {item}
-                      </DialogTitle>
-                      <DialogDescription>
-                        <Carrossel breed={item} />
-                      </DialogDescription>
-                    </DialogHeader>
-                  </DialogContent>
-                </Dialog>
-              ))}
-          </ul>
-        </div>
+        {!dogs.isLoading && (
+          <div className="flex flex-col bg-white p-6 rounded-sm">
+            <ul className=" grid grid-cols-4 gap-3">
+              {dogs.data &&
+                Object.keys(dogs.data.message).map((item, i) => (
+                  <Dialog key={i}>
+                    <DialogTrigger className="flex items-center gap-2">
+                      <li className="flex gap-2 text-gray-900 p-4 bg-neutral-100  w-full hover:bg-neutral-200 hover:cursor-pointer duration-200 rounded-sm">
+                        <Icon icon="ph:dog-bold" className="text-amber-900" />
+                        {capitalizeFirstLetter(item)}
+                      </li>
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader className="flex justify-center items-center">
+                        <DialogTitle className="py-4 text-gray-950">
+                          Fotos da raça {item}
+                        </DialogTitle>
+                        <DialogDescription asChild>
+                          <Carrossel breed={item} />
+                        </DialogDescription>
+                      </DialogHeader>
+                    </DialogContent>
+                  </Dialog>
+                ))}
+            </ul>
+          </div>
+        )}
       </main>
     </>
   );
--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -58,14 +58,14 @@ function DialogContent({
         data-slot="dialog-content"
         className={cn(
           // Alterando a cor de fundo aqui
-          "bg-neutral-100 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-3xl",
+          "bg-neutral-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-3xl",
           className
         )}
         {...props}
       >
         {children}
-        <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
-          <XIcon />
+        <DialogPrimitive.Close className="rounded-full p-1 cursor-pointer ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
+          <XIcon className="text-black rounded-sm hover:cursor-pointer" />
           <span className="sr-only">Close</span>
         </DialogPrimitive.Close>
       </DialogPrimitive.Content>
