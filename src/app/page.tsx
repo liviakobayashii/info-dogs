7ece4824cd15f4df85a69ccf22f12078b603b2d6
liviakobayashii <liviakmm10@gmail.com>
2025-03-25 18:06:28 -0300

    update: criação do componente dialog e atualização do código

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
 "use client";
 
 import { useDogs } from "@/utils/queries";
-import Link from "next/link";
 import { Icon } from "@iconify/react";
 import Header from "@/components/header";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import Carrossel from "@/components/carrossel";
+import DialogItem from "@/components/dialog";
 
 const capitalizeFirstLetter = (string: string) => {
   return string.charAt(0).toUpperCase() + string.substring(1);
@@ -31,35 +23,23 @@ export default function Page() {
         {dogs.isLoading && (
           <h2 className="text-2xl font-bold text-gray-950">Carregando...</h2>
         )}
-        {/* <Link href={"/random"}>
-        <button className=" text-white bg-amber-900 hover:bg-amber-950 hover:cursor-pointer duration-200 p-3 rounded-sm">
-          Gerar imagem aleatória
-        </button>
-      </Link> */}
 
         {!dogs.isLoading && (
           <div className="flex flex-col bg-white p-6 rounded-sm">
             <ul className=" grid grid-cols-4 gap-3">
               {dogs.data &&
                 Object.keys(dogs.data.message).map((item, i) => (
-                  <Dialog key={i}>
-                    <DialogTrigger className="flex items-center gap-2">
+                  <DialogItem
+                    key={i}
+                    title={`Fotos da raça ${item}`}
+                    trigger={
                       <li className="flex gap-2 text-gray-900 p-4 bg-neutral-100  w-full hover:bg-neutral-200 hover:cursor-pointer duration-200 rounded-sm">
                         <Icon icon="ph:dog-bold" className="text-amber-900" />
                         {capitalizeFirstLetter(item)}
                       </li>
-                    </DialogTrigger>
-                    <DialogContent>
-                      <DialogHeader className="flex justify-center items-center">
-                        <DialogTitle className="py-4 text-gray-950">
-                          Fotos da raça {item}
-                        </DialogTitle>
-                        <DialogDescription asChild>
-                          <Carrossel breed={item} />
-                        </DialogDescription>
-                      </DialogHeader>
-                    </DialogContent>
-                  </Dialog>
+                    }
+                    description={<Carrossel breed={item} />}
+                  />
                 ))}
             </ul>
           </div>
---
+++ src/components/dialog.tsx
@@ -0,0 +1,31 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { ReactNode } from "react";
+
+type Props = {
+  trigger: ReactNode;
+  title: string;
+  description: ReactNode;
+};
+
+export default function DialogItem({ trigger, title, description }: Props) {
+  return (
+    <Dialog>
+      <DialogTrigger className="flex items-center gap-2">
+        {trigger}
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader className="flex justify-center items-center">
+          <DialogTitle className="py-4 text-gray-950">{title}</DialogTitle>
+          <DialogDescription asChild>{description}</DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
